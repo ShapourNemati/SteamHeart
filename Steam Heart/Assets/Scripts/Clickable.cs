a1dffdd9f14a8a443597d04bdca376cc9bc8efda
Shapour <shapournemati@gmail.com>
2017-01-21 09:14:41 +0100

    Created Clickable interface and setup Organ script

---
+++ Steam Heart/Assets/Scripts/Clickable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public interface Clickable
+	{
+		void Clicked();
+	}
+}
+
---
+++ Steam Heart/Assets/Scripts/Organ.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AssemblyCSharp;
+
+public class Organ : MonoBehaviour, Clickable {
+
+	private ImpulseType impulseType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Clicked()
+	{
+	}
+}
