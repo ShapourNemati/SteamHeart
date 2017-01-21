0198bd4cd60a9bbf11254f764be2eab74b4f87df
LAPTOP-LSHFCGA8\overd <matteo_grilli@msn.com>
2017-01-21 11:23:40 +0100

    ScreenProperties ora espone un metodo per il calcolo della posizione in cui spawnare gli impulsi

--- Steam Heart/Assets/Scripts/ScreenProperties.cs
+++ Steam Heart/Assets/Scripts/ScreenProperties.cs
@@ -6,8 +6,16 @@ public class ScreenProperties : MonoBehaviour {
 
 	public const int maxImpulsesOnScreen = 1;
 	public const float width = 0;
+	// Hard coddato in base alle dimensioni dello sprite dell'impulso
+	public const float impulseWidth = width/maxImpulsesOnScreen;
+	private Vector3 impulseSpawnPoint;
 
-	// Dovrebbe anche avere un qualche riferimento per lo schermo
-	// tipo un punto. L'importante è che sia comodo per generare le onde.
-	// TODO !
+
+	void Awake() {
+		impulseSpawnPoint = transform.position + Vector3.right * (width / 2 + impulseWidth / 2);
+	}
+
+	public Vector3 getInpulseSpawnPoint() {
+		return impulseSpawnPoint;
+	}
 }
