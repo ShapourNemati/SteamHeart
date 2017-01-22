faf1c563aab27acc6b38250ba7227a42ded090fd
LAPTOP-LSHFCGA8\overd <matteo_grilli@msn.com>
2017-01-22 09:08:42 +0100

    Nuova logica più consistente per trovare l'impulso corrente da premere.

---
+++ Steam Heart/Assets/Scripts/Index.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Index : MonoBehaviour {
+
+	[HideInInspector]
+	public Impulse currentImpulse;
+
+	void onTriggerEnter(Collider collider) {
+		currentImpulse = collider.gameObject.GetComponent<Impulse> ();
+	}
+}
--- Steam Heart/Assets/Scripts/WaveManager.cs
+++ Steam Heart/Assets/Scripts/WaveManager.cs
@@ -125,6 +125,6 @@ public class WaveManager : MonoBehaviour {
 
 	public void OrganClickNotice(ImpulseType clickedType) {
 //		impulsesQueue [0].GetComponent<Impulse> ().resolveImpulse (clickedType, currentPattern);
-		// Usiamo la freccia TODO
+		GameObject.Find("Index").GetComponent<Index>().currentImpulse.resolveImpulse(clickedType);
 	}
 }
